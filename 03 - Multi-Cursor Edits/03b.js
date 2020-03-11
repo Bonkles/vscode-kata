92f9bbe2e70219eb45966fd1ea45ebd5d567b423
Benjamin Clark <clarkben@fb.com>
2020-03-11 13:53:09 -0400

    Add kata 03, tweaks to kata 02.

---
+++ 03 - Multi-Cursor Edits/03b.js
@@ -0,0 +1,33 @@
+//3b part 1 -  rename all instances of myVar with a double-click,
+//then use the 'Add Selection To Next Find Match' (⌘+d) to select them one at a time.
+//You can also use (⌘+⇧+L) to select them all at once.
+function refactorMeh(){
+    var myVar = myVarFromFactoryMethod();
+    myVar++;
+    myVar--;
+    return myVar;
+}
+
+//3b Part 2 - use the built-in refactoring (F2).
+function refactorMeh(){
+    var myVar = myVarFromFactoryMethod();
+    myVar++;
+    myVar--;
+    return myVar;
+}
+
+// 3b Part 3 - Say that these library method calls
+// were refactored to no longer have a prefix of '_lb'.
+// You could just rename them individually one by one.
+// In this case, let's practice selecting with ⌘+d, and
+// a selection with (⌘+k ⌘+d).
+
+// Use the 'Keyboard Shortcuts' in the VSCode settings
+// and click the ⌨︎ to test out your keyboard skillz.
+function computeContestWinner(){
+    var lb_db_results = getResultFromDB();
+    var lb_web_results = getResultFromWeb();
+    var lb_user_results = getResultFromUser();
+
+    computeSomething(lb_db_results, lb_web_results, lb_user_results);
+}
