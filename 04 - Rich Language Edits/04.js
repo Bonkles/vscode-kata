6e68e9307fd21664482eee231a2bc39ffb845cb6
Benjamin Clark <clarkben@fb.com>
2020-03-12 17:54:43 -0400

    Add lots more stuff for exercise 04.

--- 04 - Rich Language Edits/04.js
+++ 04 - Rich Language Edits/04.js
@@ -15,6 +15,17 @@
       console.table(oldestInventorFirst)
 
 
+// Want to search by symbol? hit ⌘+p and type '@' for all the symbols.
+// If you want to sort symbols into 'types', add a ':' after the @.
+// To search multiple files for symbols, search for '#'.
+// Line numbers?  ⌃+g, or ⌘+p and type ':' then the number.
+
+// Fold the code! Put your cursor in the gutter and click the '⌄'
+//to hide the array.
+
+// If you select this block and do any operation, vscode will do what
+// you expected. kill the selection? copy? paste? it will grab the whole thing,
+// not just what's visible.
 
     const inventors = [
         { first: 'Albert', last: 'Einstein', year: 1879, passed: 1955 },
@@ -30,3 +41,5 @@
         { first: 'Lise', last: 'Meitner', year: 1878, passed: 1968 },
         { first: 'Hanna', last: 'Hammarström', year: 1829, passed: 1909 }
       ];
+
+// Now, there was one last thing we should show you. Open any kata file in a new window.
